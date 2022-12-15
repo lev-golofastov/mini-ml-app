52e61b3c11c57712b26e56e0cfff10a5cd9e9d30
Lev-Golofastov <l.golofastov@mail.ru>
2022-12-15 23:28:27 +0300

    feat: add min and max length slides

--- src/main.py
+++ src/main.py
@@ -3,11 +3,13 @@ import torch
 from transformers import pipeline
 
 
-def analyze_text(text_to_analyze):
+def analyze_text(text_to_analyze, min_length, max_length):
     summarizer = pipeline(
         "summarization",
         "pszemraj/long-t5-tglobal-base-16384-book-summary",
         device="cuda" if torch.cuda.is_available() else "cpu",
+        min_length=min_length,
+        max_length=max_length
     )
 
     result = summarizer(text_to_analyze)
@@ -30,13 +32,19 @@ def main():
 
     with st.form("my_form"):
         txt = st.text_area("Enter your text here:", sample)
+        summary_min_length = st.slider("You may set the minimum length of the summary:",
+                                       min_value=1, max_value=100, value=8)
+        summary_max_length = st.slider("You may set the maximum length of the summary:",
+                                       min_value=10, max_value=1024, value=512)
 
         submitted = st.form_submit_button("Submit")
         if submitted:
             if txt == "":
                 st.error("No text to analyze")
+            elif summary_min_length > summary_max_length:
+                st.error("Minimal length cannot be greater than maximum length")
             else:
-                result = analyze_text(txt)
+                result = analyze_text(txt, summary_min_length, summary_max_length)
 
                 st.write("**Result:**")
                 st.write(result)
